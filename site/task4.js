7fc919f3c7847b62ff272520b4b19436248b3c20
DaniyarMakhambetov <noisy_b0y@mail.ru>
2020-11-24 00:50:03 +0600

    asdsdgefdsd

--- site/task4.js
+++ site/task4.js
@@ -1,16 +1,16 @@
 let input = prompt("your name?")
 let array = input.split("")
-let greet = "Hello"
+let byeSpeaker = "Hello"
 let cach = "SomeName"
 let names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"]
-if (array[0]==="J") {
+if (array[0]==="J" || array[0]==="j") {
 	cach = "JSomeName"
 	for (let i = 0; i < names.length; i++){
 		if (input === names[i]) {
 			cach = names[i]
 		}
 	}
-	greet = "Goodbye"	
+	byeSpeaker = "Goodbye"	
 }
 else if (array[0]!="J") {
 	for (let i = 0; i < names.length; i++){
@@ -19,4 +19,4 @@ else if (array[0]!="J") {
 		}
 	}
 }
-console.log(greet + " " + cach)
+console.log(byeSpeaker + " " + cach)
